45ebc8361257410e04799afdee99aa998832a91d
Thomas Barr <thomas.barr1983@gmail.com>
2019-06-20 15:33:01 +0000

    nomster10

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,23 @@
 class PlacesController < ApplicationController
 
+## perform a paginated query:
+@posts = Nomster.paginate(page: params[:page])
+
+# or, use an explicit "per page" limit:
+Nomster.paginate(page: params[:page], per_page: 30)
+
+## render page links in the view:
+<%= will_paginate @posts %>
+
+
+class Nomster
+  self.per_page = 10
+end
+
+
+WillPaginate.per_page = 10
+
+
   def index
     @places = Place.all
   end
