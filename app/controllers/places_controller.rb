133dfa2b198539df8f0068f7001be841b4f3f90c
Thomas Barr <thomas.barr1983@gmail.com>
2019-06-20 17:38:16 +0000

    nomster12

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,23 @@
 class PlacesController < ApplicationController
-
   def index
-    @places = place.all
+    @places = Place.all
   end
-    
+
   def new
     @place = Place.new
   end
 
-end
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+end
 
 
 
